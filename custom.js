829d11b452ce599fa5b98e0ce6e2da78a385499b
Hadam Jang <408hdd@gmail.com>
2023-06-24 22:04:27 +0900

    modify js

--- custom.js
+++ custom.js
@@ -1,8 +1,21 @@
-let clock = document.querySelector('#clock');
+// this is clock ver.2 
+/* let clock = document.querySelector('#clock');
 
 function getClock() {
   let date = new Date();
   clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
+
+setInterval(getClock, 1000); */
+
+let clock = document.querySelector('#clock');
+
+function getClock() {
+  let date = new Date();
+  let hours = String(date.getHours()).padStart(2, '0');
+  let minutes = String(date.getMinutes()).padStart(2, '0');
+  let seconds = String(date.getSeconds()).padStart(2, '0');
+  clock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
-setInterval(getClock, 1000)
+setInterval(getClock, 1000);
